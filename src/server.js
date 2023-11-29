bbb21841064116a8e022551ba494cbc03d75e7bb
nahblue <ana.paula.alves11@gmail.com>
2023-11-29 14:11:11 -0300

    implementação do cors

--- src/server.js
+++ src/server.js
@@ -3,11 +3,13 @@ const database = require("./database/sqlite");
 const AppError = require("./utils/AppError");
 const uploadConfig = require("./configs/upload");
 
+const cors = require("cors");
 const express = require("express");
 const routes = require("./routes");
 
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
